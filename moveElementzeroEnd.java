c2e49f08ccece9b5b49af2466e93edcf7ba39de3
shivammiet <shivamwalther@gmail.com>
2023-04-16 23:23:21 +0530

    initial.commit

---
+++ moveElementzeroEnd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class moveElementzeroEnd {
+    static int pushelement(int arr[], int n){
+    int count = 0; int swap =1;
+    for(int i = 0; i <arr.length; i++)
+        if(arr[i] != 0)
+            swap(arr[i], arr[count]);
+        
+    
+
+}
+}
+
+    
